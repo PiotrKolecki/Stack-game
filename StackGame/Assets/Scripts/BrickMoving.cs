6b09bc09a72d20502c6db5bb7dc81844b9a0eb34
DorianKossowski <doriancfc@gmail.com>
2017-06-28 17:24:45 +0200

    First try with spawning

---
+++ StackGame/Assets/Scripts/BrickMoving.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickMoving : MonoBehaviour {
+
+    private Rigidbody2D rb;
+	void Start () {
+       // rb = gameObject.GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(0, -5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    void OnCollisionEnter2D(Collision2D Collision)
+    {
+        //rb.isKinematic = true;
+    }
+}
---
+++ StackGame/Assets/Scripts/SpawningBricks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningBricks : MonoBehaviour {
+
+    public GameObject [] bricks;
+    private float bottomY;
+    public GameObject bottom;
+    public float delay = 1f;
+    private float newResp = 0f;
+    private float leftEdge, rightEdge;
+    private int i;
+    void Start () {
+        bottomY = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).y;
+        Instantiate(bottom, new Vector2(0, bottomY), Quaternion.identity);
+    }
+
+    void Update () {
+        if(Time.time > newResp)
+        {
+            newResp = Time.time + delay;
+            i = Random.Range(0, 6);
+            Instantiate(bricks[i], bricks[i].transform.position, Quaternion.identity);
+        }
+	}
+}
