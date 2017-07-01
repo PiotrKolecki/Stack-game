f7a3020a335bfb4a0353a50e6bb9984439e0e2e9
DorianKossowski <doriancfc@gmail.com>
2017-07-01 14:11:38 +0200

    Fit the screen

--- StackGame/Assets/Scripts/BrickMoving.cs
+++ StackGame/Assets/Scripts/BrickMoving.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class BrickMoving : MonoBehaviour {
 
-    private Rigidbody2D rb;
-	void Start () {
-       // rb = gameObject.GetComponent<Rigidbody2D>();
-        //rb.velocity = new Vector2(0, -5);
-	}
+    //private Rigidbody2D rb;
+    private float x;
+    void Start () {
+        sizeToScreen();
+        x = transform.position.x;
+    }
 	
-	// Update is called once per frame
 	void Update () {
+        transform.position = new Vector3(x,transform.position.y, 0);
+        transform.rotation = Quaternion.identity;
 	}
     void OnCollisionEnter2D(Collision2D Collision)
     {
-        //rb.isKinematic = true;
+       // Instantiate(gameObject, new Vector3(x, transform.position.y, 0), Quaternion.identity);
+        //Destroy(gameObject);
+    }
+    void sizeToScreen() //fit scale of brick to size of screen
+    {
+        float diff;
+        SpawningBricks SBscript = GameObject.Find("GameController").GetComponent<SpawningBricks>();
+        diff = 2 * SBscript.rightEdge;
+        transform.localScale = new Vector3(diff / 6, diff / 6, 0);
     }
 }
--- StackGame/Assets/Scripts/SpawningBricks.cs
+++ StackGame/Assets/Scripts/SpawningBricks.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SpawningBricks : MonoBehaviour {
 
     public GameObject [] bricks;
-    private float bottomY;
+    private float bottomY, topY;
     public GameObject bottom;
     public float delay = 1f;
     private float newResp = 0f;
-    private float leftEdge, rightEdge;
     private int i;
-    void Start () {
+    public Text LR , size;
+    private float sizeofBrick;
+    [HideInInspector]
+    public float leftEdge, rightEdge;
+    void Awake()
+    {
+        leftEdge = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).x;
+        rightEdge = Camera.main.ViewportToWorldPoint(new Vector2(1, 0)).x;
         bottomY = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).y;
+        topY = Camera.main.ViewportToWorldPoint(new Vector2(0, 1)).y;
+    }
+    void Start () {
+
         Instantiate(bottom, new Vector2(0, bottomY), Quaternion.identity);
+        LR.text = "Left edge: " + leftEdge + " Right edge: " + rightEdge;
+        sizeofBrick = 2 * rightEdge / 6;
+        size.text = "" + sizeofBrick;
     }
 
     void Update () {
@@ -21,7 +36,12 @@ public class SpawningBricks : MonoBehaviour {
         {
             newResp = Time.time + delay;
             i = Random.Range(0, 6);
-            Instantiate(bricks[i], bricks[i].transform.position, Quaternion.identity);
+            if (i == 0)
+                Instantiate(bricks[i], new Vector3(leftEdge, topY + sizeofBrick, 0), Quaternion.identity);
+            else if(i==5)
+                Instantiate(bricks[i], new Vector3(rightEdge, topY + sizeofBrick, 0), Quaternion.identity);
+            else
+                Instantiate(bricks[i], new Vector3(leftEdge +sizeofBrick * (0.5f + i), topY + sizeofBrick, 0), Quaternion.identity);
         }
 	}
 }
